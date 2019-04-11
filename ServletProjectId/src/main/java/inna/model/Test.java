f34448281cb90e0d16ec26d9de4b86e222558eb0
InnaPolushkina <innachka.polushkina@gmail.com>
2019-04-11 19:33:16 +0300

    test git with idea

---
+++ ServletProjectId/src/main/java/inna/model/Test.java
@@ -0,0 +1,21 @@
+package inna.model;
+
+public class Test {
+    private int id;
+
+    public Test() {
+        super();
+    }
+
+    public Test(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
